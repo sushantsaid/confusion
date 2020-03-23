b80c0ef1e0495a3e13d32cf56d3e97e93672ef2a
sushantsaid <sushdsaid@gmail.com>
2020-03-23 16:50:07 +0530

    React router
    Added HomeComponent.js
    
    Main changes to MainComponent.js by implementing the React Router.
    
    Also implemented the navigation bar using the react router features

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,8 +1,23 @@
 import React,{Component} from 'react';
-import {Navbar,NavbarBrand,Jumbotron} from 'reactstrap';  //import the navigation bar component of reactstrap
-
+import {Navbar,NavbarBrand,Jumbotron,Nav,NavbarToggler,Collapse,NavItem} from 'reactstrap';  //import the navigation bar component of reactstrap
+import {NavLink} from 'react-router-dom';
 
 class Header extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            isNavOpen : false
+        };
+        this.toggleNav = this.toggleNavbar.bind(this);//bind the function(toggleNavbar) to the variable(toggleNav). Variable name and function name can be same
+    }
+
+    toggleNavbar(){
+        this.setState({
+            isNavOpen : ! this.state.isNavOpen 
+        });
+    }
+
     render(){
         return(
             <>
@@ -12,9 +27,39 @@ class Header extends Component{
                 We can also use a <div> tag instead of fragment.
                 div tag will add extra node to DOM tree but a fragment doesn't
             */}
-                <Navbar dark> 
+                <Navbar dark expand="md">{/*expand="md" indicates that navigation bar will be completely shown for medium to extra large devices */} 
                     <div className="container">
-                        <NavbarBrand href="/">Restaurant Confusion</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav}/>{/* Button to toggle the navbar in case of small devices */}
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img src="assets/images/logo.png" 
+                            width="41" height="30" 
+                            alt="Restaurant Con Fusion">
+                            </img>
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar> {/* In order to collapse the navbar enclose the entire navbar in Collapse */}
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/home">
+                                    <span className="fa fa-home fa-lg">Home</span>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/aboutus">
+                                    <span className="fa fa-info fa-lg">About Us</span>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/menu">
+                                    <span className="fa fa-list fa-lg">Menu</span>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/contactus">
+                                    <span className="fa fa-address-card fa-lg">Contact Us</span>
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
                 <Jumbotron>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
-
+import {Route,Switch,Redirect} from 'react-router-dom';
 class Main extends Component{
 
   constructor(props){
@@ -15,27 +16,27 @@ class Main extends Component{
     };
   }
   
-  onDishSelected(dishId){
-    this.setState({selectedDish : dishId});
-}
+
 
   render(){
+
+    const HomePage = () => {
+      return(
+        <Home/>
+      );
+    }
+
     return (
       <div>
         {/*first component is navigation bar */}
         <Header/>
-        
-        {/*second component is menu of dishes */}
-        <Menu dishes={this.state.dishes}
-        onClick={(dishId)=>this.onDishSelected(dishId)}/>
-        
-        {/*third component is dish details of selected dish */}
-        <DishDetail selectedDish={
-            this.state.dishes.filter((dish)=>dish.id === this.state.selectedDish)[0]
-            /* => allows to iterate over a javascript object(json)
-            above line will find out array of dishes whose id matches with id of selected dish id
-            here as each dish has unique id, array will contain only one element which will be at 0th index*/
-            }/>
+
+        <Switch>
+          <Route path="/home" component={HomePage}/>
+          <Route exact path = "/menu" component = {()=> <Menu dishes={this.state.dishes}/>}/>
+          <Redirect to="/home"/>{/*Default path when none of the Route paths are matched*/}
+        </Switch>
+
 
         {/*fourth component is dish details of selected dish */}
         <Footer/>
